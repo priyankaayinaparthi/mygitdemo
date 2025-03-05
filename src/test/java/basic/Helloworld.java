7f2cf5dba811067682fb2c3ad3630c56fce37ce0
priyankaayinaparthi <priyankaayinaparthi@gmail.com>
2025-03-05 17:55:28 +0530

    added method commit

--- src/test/java/basic/Helloworld.java
+++ src/test/java/basic/Helloworld.java
@@ -5,7 +5,7 @@ public class Helloworld {
 
         System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 10; i++) {
 
             System.out.println("i = " + i);
         }
